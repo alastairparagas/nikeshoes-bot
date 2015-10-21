6a06cfdf138393a92d6a90ae97304c35f819f689
Alastair Paragas <alastairparagas@gmail.com>
2015-10-21 09:19:21 -0400

    Finished Shoe Ordering

--- shoeOrderer.js
+++ shoeOrderer.js
@@ -32,30 +32,90 @@ function shoeOrderer(nikeStoreLink, somePerson) {
 		console.log(error);
 	});
 	
-	// Get correct shoe size
-	spookyBrowser.then([{
-		somePersonShoeQuantity: somePerson.shoeQuantity
-	}, function () {
-		// Get the skuAndSize value of shoe for the person's shoe size
-		var skuAndSize = this.evaluate(function () {
-			var skuAndSize;
-			$('select[name="skuAndSize"] option[name="skuId"]')
-				.each(function (index, dom) {
-					var currentSkuAndSize = $(dom).attr('value');
-					if (String(currentSkuAndSize).indexOf(
-						String(somePerson.shoeSize)) > -1) {
-						skuAndSize = currentSkuAndSize;
-					}
+	spookyBrowser
+		// Get correct shoe size
+		.then([{
+			somePersonShoeQuantity: somePerson.shoeQuantity
+		}, function () {
+			// Get the skuAndSize value of shoe for the person's shoe size
+			var skuAndSize = this.evaluate(function () {
+				var skuAndSize;
+				$('select[name="skuAndSize"] option[name="skuId"]')
+					.each(function (index, dom) {
+						var currentSkuAndSize = $(dom).attr('value');
+						if (String(currentSkuAndSize).indexOf(
+							String(somePerson.shoeSize)) > -1) {
+							skuAndSize = currentSkuAndSize;
+						}
+					});
+				return skuAndSize;
+			});
+			// Fill and submit form
+			this.fillSelectors('form.add-to-cart-form.nike-buying-tools', {
+				'select[name="skuAndSize"]': skuAndSize,
+				'select[name="qty"]': somePersonShoeQuantity
+			});
+		}])
+		// Add to Cart click
+		.thenClick('button#buyingtools-add-to-cart-button')
+		.wait(3000)
+		// Go to Cart click
+		.then(function () {
+			var url = this.evaluate(function () {
+				return $('a.header-cart-glyph-custom-class.js-header-cart-glyph-custom-class').attr('href');
+			});
+			this.open(url);
+		})
+		// Checkout click
+		.then(function () {
+			this.evaluate(function () {
+				$('input#ch4_cartCheckoutBtn').click();
+			});
+		})
+		.wait(8000)
+		// Log in to person's account
+		.then([{
+			personEmail: somePerson.nikeStoreUsername,
+			personPassword: somePerson.nikeStorePassword
+		}, function () {
+			this.fill('form#loginTunnelForm', {
+				'tunnelEmailInput': personEmail,
+				'tunnelPasswordInput': personPassword
+			});
+			this.wait(3000);
+			this.evaluate(function() {
+				$('input#ch4_loginButton').click();
+			});
+		}])
+		.wait(10000)
+		// Process CVV
+		.then([{
+			personCvv: somePerson.cvv
+		}, function () {
+			this.withFrame(2, function () {
+				this.fill('form#creditCardForm', {
+					'cvv': personCvv
 				});
-			return skuAndSize;
-		});
-		// Fill and submit form
-		this.fillSelectors('form.add-to-cart-form.nike-buying-tools', {
-			'select[name="skuAndSize"]': skuAndSize,
-			'select[name="qty"]': somePersonShoeQuantity
-		});
-	}]).thenClick('button#buyingtools-add-to-cart-button');
+				this.wait(3000);
+				this.evaluate(function () {
+					$('button.js-confirmCVV').click();
+				});
+				this.wait(4000);
+				this.evaluate(function () {
+					$('input[type="submit"][name="billSubmit"]').click();
+				});
+			});
+		}]);
+		.wait(10000)
+		// Actually place the order
+		.then(function () {
+			this.evaluate(function () {
+				$('input[type="button"].ch4_btn.ch4_btnPlaceOrder').first().click();
+			});
+		})
+		.wait(10000);
 	
+	spookyBrowser.run();
 }
 
 module.exports = exports = shoeOrderer;
